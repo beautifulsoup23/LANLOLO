7046f7a82cebbb9a9992f592595a7092ce0df983
Zixian Song <zs@lendify.se>
2017-05-08 11:46:14 +0200

    Add project files.

---
+++ WebApplication2/App_Start/IdentityConfig.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
+using Microsoft.Owin.Security;
+using WebApplication2.DAL;
+using WebApplication2.Models;
+
+
+namespace WebApplication2
+{
+    public class EmailService : IIdentityMessageService
+    {
+        public Task SendAsync(IdentityMessage message)
+        {
+            // Plug in your email service here to send an email.
+            return Task.FromResult(0);
+        }
+    }
+
+    public class SmsService : IIdentityMessageService
+    {
+        public Task SendAsync(IdentityMessage message)
+        {
+            // Plug in your SMS service here to send a text message.
+            return Task.FromResult(0);
+        }
+    }
+
+    // Configure the application user manager used in this application. UserManager is defined in ASP.NET Identity and is used by the application.
+    public class P2PUserManager : UserManager<P2PUser>
+    {
+        public P2PUserManager(IUserStore<P2PUser> store)
+            : base(store)
+        {
+        }
+
+        public static P2PUserManager Create(IdentityFactoryOptions<P2PUserManager> options, IOwinContext context) 
+        {
+            var manager = new P2PUserManager(new UserStore<P2PUser>(context.Get<P2PDbContext>()));
+            // Configure validation logic for usernames
+            manager.UserValidator = new UserValidator<P2PUser>(manager)
+            {
+                AllowOnlyAlphanumericUserNames = false,
+                RequireUniqueEmail = true
+            };
+
+            // Configure validation logic for passwords
+            manager.PasswordValidator = new PasswordValidator
+            {
+                RequiredLength = 6,
+                RequireNonLetterOrDigit = true,
+                RequireDigit = true,
+                RequireLowercase = true,
+                RequireUppercase = true,
+            };
+
+            // Configure user lockout defaults
+            manager.UserLockoutEnabledByDefault = true;
+            manager.DefaultAccountLockoutTimeSpan = TimeSpan.FromMinutes(5);
+            manager.MaxFailedAccessAttemptsBeforeLockout = 5;
+
+            // Register two factor authentication providers. This application uses Phone and Emails as a step of receiving a code for verifying the user
+            // You can write your own provider and plug it in here.
+            manager.RegisterTwoFactorProvider("Phone Code", new PhoneNumberTokenProvider<P2PUser>
+            {
+                MessageFormat = "Your security code is {0}"
+            });
+            manager.RegisterTwoFactorProvider("Email Code", new EmailTokenProvider<P2PUser>
+            {
+                Subject = "Security Code",
+                BodyFormat = "Your security code is {0}"
+            });
+            manager.EmailService = new EmailService();
+            manager.SmsService = new SmsService();
+            var dataProtectionProvider = options.DataProtectionProvider;
+            if (dataProtectionProvider != null)
+            {
+                manager.UserTokenProvider = 
+                    new DataProtectorTokenProvider<P2PUser>(dataProtectionProvider.Create("ASP.NET Identity"));
+            }
+            return manager;
+        }
+    }
+
+    // Configure the application sign-in manager which is used in this application.
+    public class P2PSignInManager : SignInManager<P2PUser, string>
+    {
+        public P2PSignInManager(P2PUserManager userManager, IAuthenticationManager authenticationManager)
+            : base(userManager, authenticationManager)
+        {
+        }
+
+        public override Task<ClaimsIdentity> CreateUserIdentityAsync(P2PUser user)
+        {
+            return user.GenerateUserIdentityAsync((P2PUserManager)UserManager);
+        }
+
+        public static P2PSignInManager Create(IdentityFactoryOptions<P2PSignInManager> options, IOwinContext context)
+        {
+            return new P2PSignInManager(context.GetUserManager<P2PUserManager>(), context.Authentication);
+        }
+    }
+}
---
+++ WebApplication2/DAL/IdentityModels.cs
@@ -0,0 +1,19 @@
+﻿using System.Data.Entity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace WebApplication2.DAL
+{
+
+    public class P2PDbContext : IdentityDbContext<P2PUser>
+    {
+        public P2PDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static P2PDbContext Create()
+        {
+            return new P2PDbContext();
+        }
+    }
+}
